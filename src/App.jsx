2e7263a1855c2b7792654a82993f0c05e98965e0
Father-rbb <samaddolapo18@gmail.com>
2026-05-27 21:22:10 +0100

    Fix folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
 import Planets from './components/Planets/Planets'
-import ContactForm from './components/Contactform/ContactForm'
+import ContactForm from './components/ContactForm/ContactForm'
 import Footer from './components/Footer/Footer'
 
 function App() {
