9c74a41342051b336cbc53f57f747d85a76dacff
Father-rbb <samaddolapo18@gmail.com>
2026-05-30 22:42:36 +0100

    Making necessary adjustment to all the horizontal line

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
 import PlanetaryVideo from './components/PlanetaryVideo/PlanetaryVideo'
+import PlanetaryComparison from './components/PlanetaryComparison/PlanetaryComparison'
 import PlanetaryFacts from './components/PlanetaryFacts/PlanetaryFacts'
 import ContactForm from './components/ContactForm/ContactForm'
 import Footer from './components/Footer/Footer'
@@ -11,6 +12,7 @@ function App() {
       <Navbar />
       <Hero />
       <PlanetaryVideo />
+      <PlanetaryComparison />
       <PlanetaryFacts />
       <ContactForm />
       <Footer />
