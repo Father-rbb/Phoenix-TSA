a8c8dc6773ef659e2287ee2488bf1ba3b123865c
Father-rbb <samaddolapo18@gmail.com>
2026-05-30 21:16:12 +0100

    Making necessary adjustment to all the horizontal line

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -9,7 +9,7 @@ function ContactForm() {
       </>
 
   
-  )
+  );
 }
 
 export default ContactForm
--- src/components/PlanetaryComparison/PlanetaryComparison.jsx
+++ src/components/PlanetaryComparison/PlanetaryComparison.jsx
@@ -1,9 +1,15 @@
 function PlanetaryComparison() {
   return (
+    <>
     <section id="planetary-comparison">
       <h2>Planetary Comparison</h2>
     </section>
-  )
-}
 
+    <hr />
+      </>
+
+  
+  );
+}
+  
 export default PlanetaryComparison
