5a58f3cd80d1ec9345617447bd9a224ebba80255
Inyiama John Chiedoziem <johninyiama@gmail.com>
2026-05-30 16:21:43 +0100

    feat: Add PlanetaryFacts table component only

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
 import PlanetaryVideo from './components/PlanetaryVideo/PlanetaryVideo'
-import PlanetaryComparison from './components/PlanetaryComparison/PlanetaryComparison'
 import PlanetaryFacts from './components/PlanetaryFacts/PlanetaryFacts'
 import ContactForm from './components/ContactForm/ContactForm'
 import Footer from './components/Footer/Footer'
@@ -12,7 +11,6 @@ function App() {
       <Navbar />
       <Hero />
       <PlanetaryVideo />
-      <PlanetaryComparison />
       <PlanetaryFacts />
       <ContactForm />
       <Footer />
