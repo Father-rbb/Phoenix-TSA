ecc96a992ce046b4a1ef9a6a2d0d3aaa725ee4dc
Abdul Sobur Ademoye <Gbeminiyiask@gmail.com>
2026-06-01 00:17:55 +0100

    Added new content for planetary video section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,9 +1,16 @@
 function Hero() {
   return (
     <section id="hero">
-      <h2>Hero Section</h2>
+      <video src=""></video>
+      <h2>How Planetary Data Helps Us Understand Space</h2>
+      <p>Planetary science goes beyond images. Comparing <span>mass</span>, <span>diameter</span>, <span>gravity</span>, and <span>density</span>, we gain insight into how planets form, behave, and interact within the solar system.</p>
     </section>
-  )
+
+     <hr />
+      </>
+
+  
+  );
 }
 
 export default Hero
