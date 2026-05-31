e490ba9a4b005db6667b29c471df056caf71c107
Abdul Sobur Ademoye <Gbeminiyiask@gmail.com>
2026-06-01 00:13:06 +0100

    Added new content
    Added new content for planetary video section

---
+++ src/components/PlanetaryVideo/PlanetaryVideo.jsx
@@ -0,0 +1,16 @@
+function PlanetaryVideo() {
+  return (
+    <>
+    <section id="PlanetaryVideo">
+      <div>
+        <video src="https://www.youtube.com/watch?v=iqJjTeYv5-M&t=1s"></video>
+      <h2>Planetary science goes beyond images. Comparing <span>mass</span>, <span>diameter</span>, <span>gravity</span>, and <span>density</span>, we gain insight into how planets form, behave, and interact within the solar system.</h2>
+      </div>
+    </section>
+     <hr />
+      </>
+      
+  );
+}
+
+export default PlanetaryVideo
