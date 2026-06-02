3d7a3d191874a475ad9ed8406ca065158e32c796
Reho-TheLegend <rehorocks2007@gmail.com>
2026-06-02 14:04:47 +0100

    Footer.jsx
    Footer for our Capstone Project

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,40 @@
+function Footer() {
+  return (
+    <section id="footer">
+      <h2>Footer Section</h2>
+
+  
+
+  <!-- Footer -->
+<footer class="footer">
+  <div class="footer-content">
+    <div class="footer-top">
+      <div class="footer-about">
+        <h3 class="footer-title">About</h3>
+      </div>
+      <div class="popup-names" id="memberNames">
+        <!-- Names will be inserted here by JavaScript -->`
+      </div>
+    </div>
+
+    <div class="footer-divider"></div>
+
+
+
+    <div class="footer-bottom">
+      <div class="footer-credits">
+        <p class="footer-copyright">©2026 Design by <a href="https://amakandukwu.com/" class="footer-link">Amaka</a> & <a href="https://www.linkedin.com/in/ifeomaokocha" class="footer-link">Ifeoma A.</a><br>
+        Built by <a href="https://github.com/Father-rbb/Phoenix-TSA" class="footer-link">Phoenix</a>. All rights reserved</p>
+      </div>
+      <div class="footer-brand">
+        <p class="footer-branding"><a href="https://tsacademyonline.com/" class="footer-link">TSAcademy</a></p>
+      </div>
+    </div>
+  </div>
+</footer>
+
+<script src="footer.js"></script>
+
+    </section>
+
+export default Footer;
