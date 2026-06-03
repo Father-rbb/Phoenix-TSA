b91f2a5a895aba451954a867a17aca5eda137fa2
Inyiama John Chiedoziem <johninyiama@gmail.com>
2026-06-03 11:08:45 +0100

    feat: add Footer component with team credits for Phoenix Group 16

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,42 +1,48 @@
+import "./Footer.css";
+
 function Footer() {
   return (
-    <section id="footer">
-      <h2>Footer Section</h2>
-
-  
-
-  <!-- Footer -->
-<footer class="footer">
-  <div class="footer-content">
-    <div class="footer-top">
-      <div class="footer-about">
-        <h3 class="footer-title">About</h3>
+    <footer id="footer">
+      <div className="footer-top">
+        <div className="footer-about">
+          <h3>About</h3>
+          <p>
+            We are a team of frontend developers built through the TS Academy programme.
+            <br />Team members: Abdulsamad, John, Abdul-sobur, Amirat, Ayomide, Patience, Chibueze, Gift,  Mariam, Rehoboth, Uchechi, Victor.
+          </p>
+        </div>
       </div>
-      <div class="popup-names" id="memberNames">
-        <!-- Names will be inserted here by JavaScript -->`
+
+      <div className="footer-divider"></div>
+
+      <div className="footer-bottom">
+        <div className="footer-left">
+          <p>
+            &copy;2026 Design by{" "}
+            <a href="https://amakandukwu.com/" target="_blank" rel="noreferrer">
+              Amaka
+            </a>{" "}
+            &amp;{" "}
+            <a href="https://www.linkedin.com/in/ifeomaokocha" target="_blank" rel="noreferrer">
+              Ifeoma A.
+            </a>
+          </p>
+          <p>
+            Built by{" "}
+            <a href="https://github.com/Father-rbb/Phoenix-TSA" target="_blank" rel="noreferrer">
+              PHOENIX GROUP 16
+            </a>
+            . All rights reserved
+          </p>
+        </div>
+
+        <div className="footer-right">
+          <a href="https://tsacademyonline.com/" target="_blank" rel="noreferrer">
+            TSAcademy
+          </a>
+        </div>
       </div>
-    </div>
-
-    <div class="footer-divider"></div>
-
-
-
-    <div class="footer-bottom">
-      <div class="footer-credits">
-        <p class="footer-copyright">©2026 Design by <a href="https://amakandukwu.com/" class="footer-link">Amaka</a> & <a href="https://www.linkedin.com/in/ifeomaokocha" class="footer-link">Ifeoma A.</a><br>
-        Built by <a href="https://github.com/Father-rbb/Phoenix-TSA" class="footer-link">Phoenix</a>. All rights reserved</p>
-      </div>
-      <div class="footer-brand">
-        <p class="footer-branding"><a href="https://tsacademyonline.com/" class="footer-link">TSAcademy</a></p>
-      </div>
-    </div>
-  </div>
-</footer>
-
-
-
-    </section>
-    </section>
+    </footer>
   );
 }
 
